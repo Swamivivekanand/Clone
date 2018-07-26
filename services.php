a904ddfe89852c9fb686f6e916cec2187e4c5e9f
niti0801 <nitishaneha08@gmail.com>
2018-07-26 14:25:47 +0530

    26/7/18

--- services.php
+++ services.php
@@ -23,13 +23,13 @@
 						<div class="panel panel-default">
 					        <div class="panel-heading" role="tab" id="headingOne">
 					            <h4 class="panel-title">
-					                <a class="collapsed" role="button" data-toggle="	collapse" data-parent="#accordion" href="		#collapseOne" aria-expanded="true" 				aria-controls="collapseOne">
+					                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false"aria-controls="collapseOne">
 					                    <b><i class="more-less glyphicon glyphicon-plus"></i></b>
-					                    <img src="books.png">  Coaching Classes
+					                    <img src="books.png">Coaching Classes
 					                </a>
 					           	</h4>
 					        </div>
-					        <div id="collapseOne" class="panel-collapse collapse" role		="tabpanel" aria-labelledby="headingOne">
+					        <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
 					            <div class="panel-body">
 					                1st to 12th Standards ICSE/CBSE/STATE/ISC/IGSE BOARDS: COMMERCE, SCIENCE and ARTS STREAMS
 					            </div>
@@ -38,13 +38,13 @@
 					    <div class="panel panel-default">
 					        <div class="panel-heading" role="tab" id="headingTwo">
 					            <h4 class="panel-title">
-					                <a class="collapsed" role="button" data-toggle="	collapse" data-parent="#accordion" href="		#collapseTwo" aria-expanded="false" 			aria-controls="collapseTwo">
+					                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false"aria-controls="collapseTwo">
 					                	<i class="more-less glyphicon glyphicon-plus"></i>
 					                	<img src="graduation.png">Graduation
 					                </a>
 					            </h4>
 					        </div>
-					        <div id="collapseTwo" class="panel-collapse collapse" role	="tabpanel" aria-labelledby="headingTwo">
+					        <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
 					            <div class="panel-body">
 					                B.Sc, B.Com, BCA, BBA, MBA, B.E (All Branches & All subjects) M.Tech
 					            </div>
