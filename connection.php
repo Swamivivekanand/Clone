7e2f1c05cb4f1c51a8fb1033dfe798f089e4f97b
niti0801 <nitishaneha08@gmail.com>
2018-07-28 21:17:15 +0530

    28/7

--- connection.php
+++ connection.php
@@ -14,26 +14,3 @@ if(!$conn)
 }
 
 
-$sql="CREATE TABLE IF NOT EXISTS clone
-	(id INT(6) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-
-     name VARCHAR(30),
-     password VARCHAR(100),
-     email VARCHAR(30),
-     contact_no VARCHAR(15),
-     location VARCHAR(300),
-     categories INT(1),
-     salt VARCHAR(100),
-     reg_date TIMESTAMP
- )";
- 
-if(mysqli_query($conn,$sql))
-{
-	    
-}
-else
-{
-	$error_message = mysqli_error($conn);
-}
-
-?>
